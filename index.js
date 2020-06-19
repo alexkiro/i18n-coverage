bc893f0e5713df2b755c909c7160de13ddc37dd1
Alexandru Chirila <chirila.s.alexandru@gmail.com>
2020-06-19 09:47:36 +0300

    Parse po files and log coverage.

--- index.js
+++ index.js
@@ -1,25 +1,70 @@
 const core = require("@actions/core");
 const glob = require("@actions/glob");
 const github = require("@actions/github");
+const po2json = require("po2json");
+
+/**
+ * @param value {string}
+ * @return {Array}
+ */
+function getArrayInput(value) {
+  if (!value) return [];
+
+  return value.split(",")
+    .map((item) => item.trim().toLowerCase())
+    .filter((item) => !!item);
+}
+
 
 async function main() {
   const translationPath = core.getInput("translations-path") || "translations";
-  const defaultLanguage = core.getInput("default-language") || "en";
+  const onlyLanguages = getArrayInput(core.getInput("only-languages"));
+  const ignoreLanguages = getArrayInput(core.getInput("ignore-languages"));
+
+  console.log("translations-path", translationPath);
+  console.log("only-languages:", onlyLanguages);
+  console.log("ignore-languages:", ignoreLanguages);
 
   const globber = await glob.create([
-    `${translationPath}/*/*/*.po`,
-    `${translationPath}/locale/*/*/*.po`
-  ].join("\n"))
-  const files = await globber.glob()
-  files.forEach((file) => {
-    console.log("File:", file);
-  })
-  console.log("Path:", translationPath);
-  console.log("Lang:", defaultLanguage);
+    `${translationPath}/**/*.po`,
+  ].join("\n"));
 
-  core.setOutput("coverage", "100.00");
+  let totalMessages = 0;
+  let totalTranslatedMessages = 0;
+
+  (await globber.glob()).forEach((file) => {
+    console.log("Parsing file:", file);
+    const translation = po2json.parseFileSync(file);
+    const language = translation[""].language.trim().toLowerCase();
+    if (
+      (onlyLanguages.length > 0 && onlyLanguages.indexOf(language) === -1) ||
+      (ignoreLanguages.length > 0 && ignoreLanguages.indexOf(language) !== -1)
+    ) {
+      console.log("Skipping file:", file)
+      return
+    }
+
+    let poMessages = 0;
+    let poTranslatedMessages = 0;
+
+    Object.entries(translation).forEach(([msgid ,msgstr]) => {
+      if (msgid === "") return;
+      poMessages += 1;
+      poTranslatedMessages += msgstr[1] ? 1 : 0;
+    })
+    console.log(
+      `"${language}" translated ${(poTranslatedMessages / poMessages * 100).toFixed(2)}%`,
+      `(${poTranslatedMessages} / ${poMessages})`
+    )
+    totalMessages += poMessages;
+    totalTranslatedMessages += poTranslatedMessages;
+  });
+  core.setOutput(
+    "coverage",
+    (totalTranslatedMessages / totalMessages * 100).toFixed(2),
+  );
 }
 
 main().catch((error) => {
   core.setFailed(error.message);
-})
+});
