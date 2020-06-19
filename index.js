92c1b9f2e60cab654292ebbecfd9d802ad4dedde
Alexandru Chirila <chirila.s.alexandru@gmail.com>
2020-06-19 13:00:07 +0300

    Be pretty.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+/* eslint-env node */
+
 const core = require("@actions/core");
 const glob = require("@actions/glob");
 const github = require("@actions/github");
 const po2json = require("po2json");
 
 const token = core.getInput("token");
-const translationPath = core.getInput("translations-path") || "translations/**/*.po";
+const translationPath =
+  core.getInput("translations-path") || "translations/**/*.po";
 const onlyLanguages = getArrayInput(core.getInput("only-languages"));
 const ignoreLanguages = getArrayInput(core.getInput("ignore-languages"));
 
@@ -16,7 +19,6 @@ main().catch((error) => {
   core.setFailed(error.message);
 });
 
-
 /**
  * @param value {string}
  * @return {Array}
@@ -24,53 +26,55 @@ main().catch((error) => {
 function getArrayInput(value) {
   if (!value) return [];
 
-  return value.split(",")
+  return value
+    .split(",")
     .map((item) => item.trim().toLowerCase())
     .filter((item) => !!item);
 }
 
 function parseFile(file) {
-    console.log("Parsing file:", file);
-    const translation = po2json.parseFileSync(file);
-    const language = translation[""].language.trim().toLowerCase();
-    const details = {
-      language,
-      skipped: false,
-      messageCount: 0,
-      translatedMessageCount: 0,
-      summary: "",
-      coverage: 0
-    };
-    if (
-      (onlyLanguages.length > 0 && onlyLanguages.indexOf(language) === -1) ||
-      (ignoreLanguages.length > 0 && ignoreLanguages.indexOf(language) !== -1)
-    ) {
-      console.log("Skipping file:", file);
-      details.skipped = true;
-      details.summary = ` - "${file}" skipped`
-      return details;
-    }
+  console.log("Parsing file:", file);
+  const translation = po2json.parseFileSync(file);
+  const language = translation[""].language.trim().toLowerCase();
+  const details = {
+    language,
+    skipped: false,
+    messageCount: 0,
+    translatedMessageCount: 0,
+    summary: "",
+    coverage: 0,
+  };
+  if (
+    (onlyLanguages.length > 0 && onlyLanguages.indexOf(language) === -1) ||
+    (ignoreLanguages.length > 0 && ignoreLanguages.indexOf(language) !== -1)
+  ) {
+    console.log("Skipping file:", file);
+    details.skipped = true;
+    details.summary = ` - "${file}" skipped`;
+    return details;
+  }
 
-    Object.entries(translation).forEach(([msgid, msgstr]) => {
-      if (msgid === "") return;
-      details.messageCount += 1;
-      details.translatedMessageCount += msgstr[1] ? 1 : 0;
-    });
+  Object.entries(translation).forEach(([msgid, msgstr]) => {
+    if (msgid === "") return;
+    details.messageCount += 1;
+    details.translatedMessageCount += msgstr[1] ? 1 : 0;
+  });
 
-    details.coverage = (details.translatedMessageCount / details.messageCount * 100)
-      .toFixed(2);
-    details.summary = ` - "${file}" translated ${details.coverage} `;
-    details.summary += `(${details.translatedMessageCount} / ${details.messageCount} messages)`;
+  details.coverage = (
+    (details.translatedMessageCount / details.messageCount) *
+    100
+  ).toFixed(2);
+  details.summary = ` - "${file}" translated ${details.coverage} `;
+  details.summary += `(${details.translatedMessageCount} / ${details.messageCount} messages)`;
 
-    console.log(details.summary);
-    return details
+  console.log(details.summary);
+  return details;
 }
-
 
 async function main() {
   const globber = await glob.create(translationPath);
 
-  let allSummaries = []
+  let allSummaries = [];
   let totalMessages = 0;
   let totalTranslatedMessages = 0;
 
@@ -81,9 +85,8 @@ async function main() {
     totalTranslatedMessages += details.translatedMessageCount;
   });
 
-  const coverage = (totalTranslatedMessages / totalMessages * 100).toFixed(2);
-  const summary =
-    `Total coverage ${coverage}% (${totalTranslatedMessages} / ${totalMessages} messages)`;
+  const coverage = ((totalTranslatedMessages / totalMessages) * 100).toFixed(2);
+  const summary = `Total coverage ${coverage}% (${totalTranslatedMessages} / ${totalMessages} messages)`;
 
   console.log(summary);
   core.setOutput("coverage", coverage);
@@ -93,19 +96,17 @@ async function main() {
 
   console.log("Creating check run");
   const octokit = github.getOctokit(token);
-  octokit.checks.create(
-    {
-      owner: context.repository.owner.login,
-      repo: context.repository.name,
-      name: "i18n-coverage",
-      head_sha: context.head_commit.id,
-      status: "completed",
-      conclusion: "neutral",
-      output: {
-        title: `I18N: ${coverage}%`,
-        summary: summary,
-        text: allSummaries.join("\n")
-      }
-    }
-  );
+  octokit.checks.create({
+    owner: context.repository.owner.login,
+    repo: context.repository.name,
+    name: "i18n-coverage",
+    head_sha: context.head_commit.id,
+    status: "completed",
+    conclusion: "neutral",
+    output: {
+      title: `I18N: ${coverage}%`,
+      summary: summary,
+      text: allSummaries.join("\n"),
+    },
+  });
 }
